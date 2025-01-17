869375bbec974b3460a6cba9abc7082f35efc0ea
ucangun <149247682+ucangun@users.noreply.github.com>
2025-01-17 13:39:26 +0100

    dfsgdsfg

--- index.js
+++ index.js
@@ -1,65 +1,56 @@
-"use strict"
+"use strict";
 
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
 /* ----------------------------------- */
 // Required Modules:
 
 // envVariables to process.env:
-require("dotenv").config()
-const HOST = process.env?.HOST || '127.0.0.1'
-const PORT = process.env?.PORT || 8000
+require("dotenv").config();
+const HOST = process.env?.HOST || "127.0.0.1";
+const PORT = process.env?.PORT || 8000;
 
 // asyncErrors to errorHandler:
-require('express-async-errors')
+require("express-async-errors");
 
 /* ----------------------------------- */
 // Configrations:
 
 // Connect to DB:
-const { dbConnection } = require('./src/configs/dbConnection')
-dbConnection()
+const { dbConnection } = require("./src/configs/dbConnection");
+dbConnection();
 
 /* ------------------------------------------------------- */
 // Middlewares:
 
 // Accept JSON:
-app.use(express.json())
-
-
-
+app.use(express.json());
 
 /* ------------------------------------------------------- */
-
-
-
-
 
 /* ------------------------------------------------------- */
 // Routes:
 
 // HomePath:
-app.all('/', (req, res) => {
-    res.send({
-        error: false,
-        message: 'Welcome to Soul Journey API'
-    })
-})
-
-
+app.all("/", (req, res) => {
+  res.send({
+    error: false,
+    message: "Welcome to Soul Journey API",
+  });
+});
 
 // Not Found
-app.use('*', (req, res) => {
-    res.status(404).json({
-        error: true,
-        message: '404 Not Found'
-    })
-})
+app.use("*", (req, res) => {
+  res.status(404).json({
+    error: true,
+    message: "404 Not Found",
+  });
+});
 
-
+xcvbhfgh;
 
 /* ------------------------------------------------------- */
 // RUN SERVER:
-app.listen(PORT, HOST, () => console.log(`http://${HOST}:${PORT}`))
+app.listen(PORT, HOST, () => console.log(`http://${HOST}:${PORT}`));
 /* ------------------------------------------------------- */
