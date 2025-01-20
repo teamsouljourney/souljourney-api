fd7bc14f861b29fb58d54e52fabc0ee2d5cb395c
ucangun <149247682+ucangun@users.noreply.github.com>
2025-01-20 23:36:34 +0100

    feat:Added new signup functionality to auth controller with email verification

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,3 +3,39 @@
 /* ------------------------------------------------- */
 /*                  SOULJOURNEY API                  */
 /* ------------------------------------------------- */
+
+// Auth Controller:
+
+const User = require("../models/user");
+const { signVerificationToken } = require("../helpers/jwtFunctions");
+const sendEmail = require("../helpers/sendEmail");
+
+module.exports = {
+  signup: async (req, res) => {
+    const newUser = await User.create({
+      username: req.body.username,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+      isVerified: false,
+    });
+
+    const verificationToken = signVerificationToken(newUser._id);
+
+    const verificationUrl = `${process.env.SERVER_URL}/auth/verify-email?token=${verificationToken}`;
+
+    const message = `Click the following link to verify your email address: ${verificationUrl}`;
+
+    await sendEmail({
+      email: newUser.email,
+      subject: "Verify Your Email",
+      message,
+    });
+
+    res.status(201).json({
+      status: "success",
+      message: "A verification email has been sent to your email address.",
+    });
+  },
+};
---
+++ src/routes/auth.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/* ------------------------------------------------- */
+/*                  BLOGMOTION API                   */
+/* ------------------------------------------------- */
+
+const router = require("express").Router();
+
+/* ------------------------------------------------- */
+
+const { signup } = require("../controllers/auth");
+
+router.post("/signup", signup);
+
+/* ------------------------------------------------- */
+
+module.exports = router;
