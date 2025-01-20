b550d4c605d44ad5a3d65d19d3b56c33f9448c7f
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-01-20 18:03:08 +0300

    feat: Added resetTokenHash function to helpers folder

---
+++ src/helpers/resetTokenHash.js
@@ -0,0 +1,15 @@
+"use strict";
+/* ------------------------------------------------- */
+/*                  SOULJOURNEY API                  */
+/* ------------------------------------------------- */
+
+const crypto = require("node:crypto")
+/**
+ * Hashes a given input string using SHA-256
+ * @param {string} input - The input string to hash
+ * @returns {string} - The hashed value
+*/
+
+module.exports = (input) => {
+    return crypto.createHash("sha256").update(input).digest("hex")
+}
