f8ce6a70b2a2ba29ef9d7ba36c273cdd80786ea4
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-01-21 00:45:41 +0300

    feat: Added user controller with list, create, read, update and delete methods

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,40 @@
 "use strict";
 /* -------------------------------------------------------
-                    SOULJOURNEY API  
+SOULJOURNEY API  
 ------------------------------------------------------- */
+const User = require("../models/user");
 
+module.exports = {
+    list: async (req, res) => {
+
+        const data = await res.getModelList(User)
+        res.status(200).send({
+            error: false,
+            data
+        })
+    },
+    create: async (req, res) => {
+        res.status(200).send({
+            error: false,
+            data
+        })
+    },
+    read: async (req, res) => {
+        res.status(200).send({
+            error: false,
+            data
+        })
+    },
+    update: async (req, res) => {
+        res.status(200).send({
+            error: false,
+            data
+        })
+    },
+    delete: async (req, res) => {
+        res.status(200).send({
+            error: false,
+            data
+        })
+    },
+}
