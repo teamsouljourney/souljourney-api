fed7a06b8e3602b75cbd89cd3a629b03ee4e9554
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-01-21 02:04:47 +0300

    feat: Added Swagger documentation comments to user controller methods

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -8,6 +8,19 @@ const jwt = require("jsonwebtoken")
 
 module.exports = {
     list: async (req, res) => {
+        /* 
+            #swagger.tags = ["Users"]
+            #swagger.summary = "List Users"
+            #swagger.description = `
+                You can use <u>filter[] & search[] & sort[] & page & limit</u> queries with endpoint.
+                <ul> Examples:
+                    <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
+                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                    <li>URL/?<b>sort[field1]=asc&sort[field2]=desc</b></li>
+                    <li>URL/?<b>limit=10&page=1</b></li>
+                </ul>
+            `
+        */
         const data = await res.getModelList(User)
         res.status(200).send({
             error: false,
@@ -16,6 +29,21 @@ module.exports = {
         })
     },
     create: async (req, res) => {
+        /* 
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Create User"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    "userName": "test",
+                    "password": "aA!123456",
+                    "email": "test@site.com",
+                    "firstName": "test",
+                    "lastName": "test",
+                }
+            }
+        */
         const userData = await User.create(req.body)
         // console.log(userData);
         
@@ -49,6 +77,10 @@ module.exports = {
         })
     },
     read: async (req, res) => {
+        /* 
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Read User"
+        */
         const data = await User.findOne({_id: req.params.id})
         res.status(200).send({
             error: false,
@@ -56,6 +88,21 @@ module.exports = {
         })
     },
     update: async (req, res) => {
+        /* 
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Update User"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    "userName": "test",
+                    "password": "aA!123456",
+                    "email": "test@site.com",
+                    "firstName": "test",
+                    "lastName": "test",
+                }
+            }
+        */
         const data = await User.updateOne({_id: req.params.id}, req.body, {runValidators: true})        
         res.status(201).send({
             error: false,
@@ -65,6 +112,10 @@ module.exports = {
         })
     },
     delete: async (req, res) => {
+        /* 
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Delete User"
+        */
         const data = await User.deleteOne({_id: req.params.id})
         res.status(data.deletedCount ? 204 : 404).send({
             error: !data.deletedCount,
