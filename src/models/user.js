4dcb3c862b8aa0ec59ec62ca3c4d72ba3e2991d5
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-01-20 13:31:52 +0300

    feat: Added user data and schema to user model

--- src/models/user.js
+++ src/models/user.js
@@ -4,3 +4,23 @@
 ------------------------------------------------------- */
 
 const {mongoose} = require("../configs/dbConnection")
+
+/* ------------------------------------------------------- *
+{
+    "userName": "admin",
+    "password": "aA?123456",
+    "email": "admin@site.com",
+    "firstName": "admin",
+    "lastName": "admin",
+    "isActive": true,
+    "isAdmin": true,
+    "isStaff": true,
+}
+/* ------------------------------------------------------- */
+
+const UserSchema = new mongoose.Schema({}, {
+    collection: 'users',
+    timestamps: true
+})
+
+module.exports = mongoose.model("User", UserSchema)
