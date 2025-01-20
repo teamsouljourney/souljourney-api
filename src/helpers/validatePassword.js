e848c404300108326644c1170c80317480d1ab1d
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-01-20 16:48:36 +0300

    feat: Added validatePassword function in helpers/validatePassword.js

---
+++ src/helpers/validatePassword.js
@@ -0,0 +1,17 @@
+"use strict";
+/* ------------------------------------------------- */
+/*                  SOULJOURNEY API                  */
+/* ------------------------------------------------- */
+const validator = require("validator")
+
+const validatePassword = (password) => {
+    validator.isStrongPassword(password, {
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1
+    })
+}
+
+module.exports = validatePassword
