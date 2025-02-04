0598029498a418b3bf61dbfd591048d84363c6cd
ucangun <149247682+ucangun@users.noreply.github.com>
2025-02-04 11:48:30 +0100

    feat: Added Swagger documentation to appointment controller methods

--- src/controllers/appointment.js
+++ src/controllers/appointment.js
@@ -8,6 +8,11 @@ const Appointment = require("../models/appointment");
 
 module.exports = {
   list: async (req, res) => {
+    /*
+        #swagger.tags = ['Appointment']
+        #swagger.summary = 'Get all appointments'
+        #swagger.description = 'Fetch a list of all appointments, with optional userId and therapistId population.'
+    */
     const data = await res.getModelList(Appointment, {}, "userId therapistId");
     res.status(200).send({
       error: false,
@@ -17,6 +22,22 @@ module.exports = {
   },
 
   create: async (req, res) => {
+    /*
+            #swagger.tags = ['Appointment']
+            #swagger.summary = 'Create a new appointment'
+            #swagger.description = 'Create and save a new appointment in the database.'
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                description: 'Appointment data to create.',
+                schema: {
+                  userId: 'string',
+                  therapistId: 'string',
+                  appointmentDate: 'date',
+                  endTime: 'date',
+              },
+          }
+    */
     const data = await Appointment.create(req.body);
     res.status(201).send({
       error: false,
@@ -25,6 +46,17 @@ module.exports = {
   },
 
   read: async (req, res) => {
+    /*
+            #swagger.tags = ['Appointment']
+            #swagger.summary = 'Get appointment by ID'
+            #swagger.description = 'Fetch an appointment by its unique ID.'
+            #swagger.parameters['id'] = {
+                in: 'path',
+                required: true,
+                description: 'ID of the appointment to retrieve.',
+                type: 'string',
+            }
+      */
     const data = await Appointment.findOne({ _id: req.params.id }).populate(
       "userId therapistId"
     );
@@ -35,6 +67,29 @@ module.exports = {
   },
 
   update: async (req, res) => {
+    /*
+            #swagger.tags = ['Appointment']
+            #swagger.summary = 'Update appointment by ID'
+            #swagger.description = 'Update an appointment\'s details by its unique ID.'
+            #swagger.parameters['id'] = {
+                in: 'path',
+                required: true,
+                description: 'ID of the appointment to update.',
+                type: 'string',
+            }
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                description: 'Updated appointment data.',
+                schema: {
+                  userId: 'string',
+                  therapistId: 'string',
+                  appointmentDate: 'date',
+                  endTime: 'date',
+              },
+          }
+    */
+
     const data = await Appointment.updateOne({ _id: req.params.id }, req.body, {
       runValidators: true,
     });
@@ -46,6 +101,17 @@ module.exports = {
   },
 
   delete: async (req, res) => {
+    /*
+            #swagger.tags = ['Appointment']
+            #swagger.summary = 'Delete appointment by ID'
+            #swagger.description = 'Delete an appointment from the database by its unique ID.'
+            #swagger.parameters['id'] = {
+                in: 'path',
+                required: true,
+                description: 'ID of the appointment to delete.',
+                type: 'string',
+            }
+    */
     const data = await Appointment.deleteOne({ _id: req.params.id });
 
     res.status(data.deletedCount ? 204 : 404).send({
