51a8c0b479faaf39a622f8a5d8d7c9d3dcdce2d3
ucangun <149247682+ucangun@users.noreply.github.com>
2025-02-05 14:32:23 +0100

    feat: Refactor therapistTimeTable controller to use TherapistTimeTable model and add swagger

--- src/controllers/therapistTimeTable.js
+++ src/controllers/therapistTimeTable.js
@@ -4,20 +4,45 @@
 /*                  SOULJOURNEY API                  */
 /* ------------------------------------------------- */
 
-const Appointment = require("../models/appointment");
+const TherapistTimeTable = require("../models/therapistTimeTable");
 
 module.exports = {
   list: async (req, res) => {
-    const data = await res.getModelList(Appointment, {}, "therapistId");
+    /*
+        #swagger.tags = ['TherapistTimeTable']
+        #swagger.summary = 'Get all therapist time tables'
+        #swagger.description = 'Fetch a list of all therapist time tables, with optional therapistId population.'
+    */
+    const data = await res.getModelList(TherapistTimeTable, {}, "therapistId");
     res.status(200).send({
       error: false,
-      details: await res.getModelListDetails(Appointment),
+      details: await res.getModelListDetails(TherapistTimeTable),
       data,
     });
   },
 
   create: async (req, res) => {
-    const data = await Appointment.create(req.body);
+    /*
+        #swagger.tags = ['TherapistTimeTable']
+        #swagger.summary = 'Create a new therapist time table'
+        #swagger.description = 'Create and save a new therapist time table in the database.'
+        #swagger.parameters['body'] = {
+            in: 'body',
+            required: true,
+            description: 'Therapist time table data to create.',
+            schema: {
+              therapistId: 'string',
+              availableDates: [
+                {
+                  date: 'string (ISO format)',
+                  startTime: 'string (HH:mm)',
+                  endTime: 'string (HH:mm)'
+                }
+              ]
+            }
+        }
+    */
+    const data = await TherapistTimeTable.create(req.body);
     res.status(201).send({
       error: false,
       data,
@@ -25,9 +50,20 @@ module.exports = {
   },
 
   read: async (req, res) => {
-    const data = await Appointment.findOne({ _id: req.params.id }).populate(
-      "userId therapistId"
-    );
+    /*
+        #swagger.tags = ['TherapistTimeTable']
+        #swagger.summary = 'Get therapist time table by ID'
+        #swagger.description = 'Fetch a therapist time table by its unique ID.'
+        #swagger.parameters['id'] = {
+            in: 'path',
+            required: true,
+            description: 'ID of the therapist time table to retrieve.',
+            type: 'string',
+        }
+    */
+    const data = await TherapistTimeTable.findOne({
+      _id: req.params.id,
+    }).populate("therapistId");
     res.status(200).send({
       error: false,
       data,
@@ -35,19 +71,57 @@ module.exports = {
   },
 
   update: async (req, res) => {
-    const data = await Appointment.updateOne({ _id: req.params.id }, req.body, {
-      runValidators: true,
-    });
+    /*
+        #swagger.tags = ['TherapistTimeTable']
+        #swagger.summary = 'Update therapist time table by ID'
+        #swagger.description = 'Update a therapist time table details by its unique ID.'
+        #swagger.parameters['id'] = {
+            in: 'path',
+            required: true,
+            description: 'ID of the therapist time table to update.',
+            type: 'string',
+        }
+        #swagger.parameters['body'] = {
+            in: 'body',
+            required: true,
+            description: 'Updated therapist time table data.',
+            schema: {
+              therapistId: 'string',
+              availableDates: [
+                {
+                  date: 'string (ISO format)',
+                  startTime: 'string (HH:mm)',
+                  endTime: 'string (HH:mm)'
+                }
+              ]
+            }
+        }
+    */
+    const data = await TherapistTimeTable.updateOne(
+      { _id: req.params.id },
+      req.body,
+      { runValidators: true }
+    );
     res.status(202).send({
       error: false,
       data,
-      new: await Appointment.findOne({ _id: req.params.id }),
+      new: await TherapistTimeTable.findOne({ _id: req.params.id }),
     });
   },
 
   delete: async (req, res) => {
-    const data = await Appointment.deleteOne({ _id: req.params.id });
-
+    /*
+        #swagger.tags = ['TherapistTimeTable']
+        #swagger.summary = 'Delete therapist time table by ID'
+        #swagger.description = 'Delete a therapist time table from the database by its unique ID.'
+        #swagger.parameters['id'] = {
+            in: 'path',
+            required: true,
+            description: 'ID of the therapist time table to delete.',
+            type: 'string',
+        }
+    */
+    const data = await TherapistTimeTable.deleteOne({ _id: req.params.id });
     res.status(data.deletedCount ? 204 : 404).send({
       error: !data.deletedCount,
       data,
