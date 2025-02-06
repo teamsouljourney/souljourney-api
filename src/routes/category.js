023213ce12fdd4582d0d3a056f545ef811f83972
ucangun <149247682+ucangun@users.noreply.github.com>
2025-02-06 09:56:28 +0100

    refactor: Updated category controller and route files with minor formatting changes and added comments

--- src/routes/category.js
+++ src/routes/category.js
@@ -6,19 +6,25 @@
 
 const router = require("express").Router();
 
-const category = require("../controllers/category");
+const {
+  list,
+  create,
+  read,
+  update,
+  deleteCategory,
+} = require("../controllers/category");
 const permission = require("../middlewares/permission");
 
 // URL: /categories
 
-router.route("/").get(category.list).post(permission.isAdmin, category.create);
+router.route("/").get(list).post(permission.isAdmin, create);
 
 router
   .route("/:id")
-  .get(category.read)
-  .put(permission.isAdmin, category.update)
-  .patch(permission.isAdmin, category.update)
-  .delete(permission.isAdmin, category.delete);
+  .get(read)
+  .put(permission.isAdmin, update)
+  .patch(permission.isAdmin, update)
+  .delete(permission.isAdmin, deleteCategory);
 
 /* ------------------------------------------------------- */
 // Exports:
