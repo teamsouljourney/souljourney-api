ad4fcfeaeca235f3698f2124c3da1ed7d91f7804
ucangun <149247682+ucangun@users.noreply.github.com>
2025-02-06 09:49:46 +0100

    feat: Added isAdmin middleware to permission.js file

--- src/middlewares/permission.js
+++ src/middlewares/permission.js
@@ -1,1 +1,21 @@
 "use strict";
+
+/* ------------------------------------------------- */
+/*                 SOULJOURNEY API                   */
+/* ------------------------------------------------- */
+
+const CustomError = require("../errors/customError");
+
+module.exports = {
+  isAdmin: (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new CustomError(
+        "NoPermission: You must login and to be Admin.",
+        400
+      );
+    }
+  },
+};
