1a25683cf2aed3b86a67304ff6d9713ded96299c
ucangun <149247682+ucangun@users.noreply.github.com>
2025-03-03 00:40:23 +0100

    feat: Refactor queryHandler middleware to update query parameters and add default sort order

--- src/middlewares/queryHandler.js
+++ src/middlewares/queryHandler.js
@@ -1,57 +1,55 @@
-"use strict"
+"use strict";
 
 module.exports = (req, res, next) => {
+  const filter = req.query?.filter || {};
 
-    
-    const filter = req.query?.filter || {}
-    
-    const search = req.query?.search || {}
-   
-    for (let key in search) search[key] = { $regex: search[key], $options: 'i' } 
-   
-    const sort = req.query?.sort || {}
-    
-    let limit = Number(req.query?.limit)
-    
-    limit = limit > 0 ? limit : Number(process.env.PAGE_SIZE || 20)
-   
+  const search = req.query?.search || {};
 
-    let page = Number(req.query?.page)
-    page = page > 0 ? (page - 1) : 0
-    
+  for (let key in search) search[key] = { $regex: search[key], $options: "i" };
 
-    let skip = Number(req.query?.skip)
-    skip = skip > 0 ? skip : (page * limit)
+  const sort = req.query?.sort || { createdAt: -1 };
 
-   
-    
-    res.getModelList = async (Model, customFilter = {}, populate = null) => {
-        return await Model.find({ ...filter, ...search, ...customFilter }).sort(sort).skip(skip).limit(limit).populate(populate)
-    }
+  let limit = Number(req.query?.limit);
 
-    res.getModelListDetails = async (Model, customFilter = {}) => {
+  limit = limit > 0 ? limit : Number(process.env.PAGE_SIZE || 20);
 
-        const data = await Model.find({ ...filter, ...search, ...customFilter })
+  let page = Number(req.query?.page);
+  page = page > 0 ? page - 1 : 0;
 
-        let details = {
-            filter,
-            search,
-            sort,
-            skip,
-            limit,
-            page,
-            pages: {
-                previous: (page > 0 ? page : false),
-                current: page + 1,
-                next: page + 2,
-                total: Math.ceil(data.length / limit)
-            },
-            totalRecords: data.length,
-        }
-        details.pages.next = (details.pages.next > details.pages.total ? false : details.pages.next)
-        if (details.totalRecords <= limit) details.pages = false
-        return details
-    }
-    
-    next()
-}
+  let skip = Number(req.query?.skip);
+  skip = skip > 0 ? skip : page * limit;
+
+  res.getModelList = async (Model, customFilter = {}, populate = null) => {
+    return await Model.find({ ...filter, ...search, ...customFilter })
+      .sort(sort)
+      .skip(skip)
+      .limit(limit)
+      .populate(populate);
+  };
+
+  res.getModelListDetails = async (Model, customFilter = {}) => {
+    const data = await Model.find({ ...filter, ...search, ...customFilter });
+
+    let details = {
+      filter,
+      search,
+      sort,
+      skip,
+      limit,
+      page,
+      pages: {
+        previous: page > 0 ? page : false,
+        current: page + 1,
+        next: page + 2,
+        total: Math.ceil(data.length / limit),
+      },
+      totalRecords: data.length,
+    };
+    details.pages.next =
+      details.pages.next > details.pages.total ? false : details.pages.next;
+    if (details.totalRecords <= limit) details.pages = false;
+    return details;
+  };
+
+  next();
+};
