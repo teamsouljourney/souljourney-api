f819dc2ac00c5aec07b31e66d5bf41b0f8e1fb41
ucangun <149247682+ucangun@users.noreply.github.com>
2025-03-18 02:25:19 +0100

    feat: Add multer middleware for file uploads

---
+++ src/middlewares/upload.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/* ------------------------------------------------- */
+/*                 SOULJOURNEY API                   */
+/* ------------------------------------------------- */
+
+// UPLOAD (Multer Middleware)
+// npm i multer
+
+const multer = require("multer");
+
+module.exports = multer({
+  //  dest: "./uploads/"
+  storage: multer.diskStorage({
+    destination: "./uploads",
+    filename: (req, file, returnCallback) => {
+      //   console.log("file", file);
+      // returnCallback(error ,filename) syntax
+      returnCallback(null, Date.now() + "_" + file.originalname);
+    },
+  }),
+});
+
+// .post(upload.single(), create);
+// .post(upload.array("image"), create)
+// .post(upload.any(), create)
