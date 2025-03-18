e533e14a73bc0a43cf5f46db4f98d71a410f235c
ucangun <149247682+ucangun@users.noreply.github.com>
2025-03-18 02:08:36 +0100

    refactor: Update database and socket configuration files with API header comments for consistency

--- src/configs/dbConnection.js
+++ src/configs/dbConnection.js
@@ -1,20 +1,22 @@
-"use strict"
-/* -------------------------------------------------------
-    | FULLSTACK TEAM | NODEJS / EXPRESS |
-------------------------------------------------------- */
+"use strict";
+
+/* ------------------------------------------------- */
+/*                  SOULJOURNEY API                  */
+/* ------------------------------------------------- */
 // MongoDB Connection:
 
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const dbConnection = function () {
-    // Connect:
-    mongoose.connect(process.env.MONGODB)
-        .then(() => console.log('* DB Connected * '))
-        .catch((err) => console.log('* DB Not Connected * ', err))
-}
+  // Connect:
+  mongoose
+    .connect(process.env.MONGODB)
+    .then(() => console.log("* DB Connected * "))
+    .catch((err) => console.log("* DB Not Connected * ", err));
+};
 
 /* ------------------------------------------------------- */
 module.exports = {
-    mongoose,
-    dbConnection
-}
+  mongoose,
+  dbConnection,
+};
