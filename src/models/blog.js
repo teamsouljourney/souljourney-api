978e62bde19ec61d18da563cc3495c7066405490
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-03-18 18:00:11 +0300

    feat: Added blog API endpoints and model implementation

--- src/models/blog.js
+++ src/models/blog.js
@@ -3,3 +3,54 @@
 /* ------------------------------------------------- */
 /*                  SOULJOURNEY API                  */
 /* ------------------------------------------------- */
+
+const { mongoose } = require("../configs/dbConnection");
+
+const BlogSchema = new mongoose.Schema({
+  therapistId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Therapist",
+      required: true
+  },
+
+  categoryId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true
+  },
+
+  title: {
+      type: String,
+      required: true,
+      trim: true
+  },
+
+  content: {
+      type: String,
+      required: [true, 'Content is required!'],
+      trim: true
+  },
+
+  image: {
+      type: String,
+      required: true,
+      trim: true
+  },
+
+  likes: [
+      {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User"
+      }
+  ],
+
+  countOfVisitors: {
+      type: Number,
+      default: 0
+  },
+}, {
+  collection: "blogs",
+  timestamps: true
+})
+
+module.exports = mongoose.model("Blog", BlogSchema);
--- src/routes/blog.js
+++ src/routes/blog.js
@@ -1,5 +1,20 @@
 "use strict";
+/* -------------------------------------------------------
+                    SOULJOURNEY API  
+------------------------------------------------------- */
 
-/* ------------------------------------------------- */
-/*                  SOULJOURNEY API                  */
-/* ------------------------------------------------- */
+const router = require("express").Router();
+
+const blog = require("../controllers/blog");
+
+router.route("/").get(blog.list).post(blog.create);
+router
+  .route("/:id")
+  .get(blog.read)
+  .put(blog.update)
+  .patch(blog.update)
+  .delete(blog.delete);
+
+router.route("/:id/likes").get(blog.getLike).post(blog.postLike);
+
+module.exports = router;
