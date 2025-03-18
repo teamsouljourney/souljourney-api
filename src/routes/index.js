08c1466e266e07696fdabc9280804eb31a88b25f
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-03-18 17:54:41 +0300

    refactor: Removed blog-related files and routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -24,8 +24,6 @@ router.use("/users", require("./user"));
 // Appointments:
 router.use("/appointments", require("./appointment"));
 
-// Blogs:
-router.use("/blogs", require("./blog"));
 
 // Categories
 router.use("/categories", require("./category"));
